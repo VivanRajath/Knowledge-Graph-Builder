c022c8e32616c352a467b2cc3b086114df9d31d4
VivanRajath <vivanrajath999@gmail.com>
2025-10-21 15:36:08 +0530

    knowldege-graph-verss-3

--- django_backend/django_backend/wsgi.py
+++ django_backend/django_backend/wsgi.py
@@ -1,5 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
+# Point to the fully-qualified settings module inside the inner package.
+# This avoids ambiguity when the repository root also contains a
+# top-level `django_backend` package.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.django_backend.settings')
 application = get_wsgi_application()
